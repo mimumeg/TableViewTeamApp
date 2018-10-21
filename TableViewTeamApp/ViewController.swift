7032ac6a95816c265db2c35f8170a14d8ffde3bb
mimumeg <meg.stella555@gmail.com>
2018-10-21 13:19:54 +0900

    first commit

---
+++ TableViewTeamApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTeamApp
+//
+//  Created by Megumi Mimura on 2018/10/21.
+//  Copyright © 2018 三村恵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
