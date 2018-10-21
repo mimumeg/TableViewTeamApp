607c198dfa94eb2e20ed8d56c27055d0037f5714
mimumeg <meg.stella555@gmail.com>
2018-10-21 13:59:36 +0900

    fix TableViewTeam

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Megumi Mimura on 2018/10/20.
+//  Copyright © 2018 三村恵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
